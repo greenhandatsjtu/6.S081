abffcfb258edfebada687b37e0dc144004b6f081
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2021-12-05 20:13:42 +0800

    finish lab 3

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -81,7 +81,34 @@ int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
-  return 0;
+  uint64 va; // starting virtual address of the first page to check
+  int n; // number of pages
+  uint64 p; //address to user space buffer
+  if(argaddr(0, &va) < 0)
+	  return -1;
+  if(argint(1, &n) < 0)
+	  return -1;
+  if(argaddr(2, &p) < 0)
+	  return -1;
+
+  // upper limit of the number of pages is 64
+  if(n > 64)
+	  return -1;
+
+  pagetable_t pagetable = myproc()->pagetable;
+//  vmprint(pagetable);
+  uint64 abits=0; // access bitmask
+
+  for(int i=0;i < n; i++){
+	  pte_t *pte = walk(pagetable, va+i*PGSIZE, 0);
+	  if(pte!=0 && (*pte&PTE_A)!=0){
+		  abits |= 1<<i; // set access bit in access bitmask
+		  *pte &= ~PTE_A; // clear PTE_A
+	  }
+  }
+
+  // copy bitmask to user space
+  return copyout(pagetable, p, (void *)&abits, sizeof(uint64));
 }
 #endif
 
