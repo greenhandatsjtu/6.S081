2b8ac0a937cfa7696966a9411c0b939d67d6a602
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2021-12-05 17:44:05 +0800

    finish ugetpid()

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,15 @@ found:
     return 0;
   }
 
+  // Allocate a USYSCALL page.
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+	  freeproc(p);
+	  release(&p->lock);
+	  return 0;
+  }
+  // set pid
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,6 +162,11 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+
+  if(p->usyscall)
+	  kfree((void*)p->usyscall);
+  p->usyscall = 0;
+
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -196,6 +210,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the USYSCALL read-only page just below TRAPFRAME.
+  // Must add PTE_U flag to allow user mode to read!!!
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+	  // free two pages, TRAPFRAME and TRAMPOLINE
+	  uvmunmap(pagetable, TRAPFRAME, 2, 0);
+	  uvmfree(pagetable,0);
+	  return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +229,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
