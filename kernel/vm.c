aa555f656a35d2bd68d23281dc837444880aadad
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2021-12-05 18:52:53 +0800

    finish vmprint()

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint_helper(pagetable_t pagetable, int level){
+	// there are 2^9 = 512 PTEs in a page table.
+	for(int i = 0; i < 512; i++){
+		pte_t pte = pagetable[i];
+		if((pte&PTE_V)){
+			// indented by ".."
+			for(int j=0;j<=level;j++){
+				if(j==0) printf("..");
+				else printf(" ..");
+			}
+			printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+			if((pte&(PTE_R|PTE_W|PTE_X)) == 0){
+				// this PTE points to a lower-level page table.
+				uint64 child = PTE2PA(pte);
+				// print child
+				vmprint_helper((pagetable_t)child, level+1);
+			}
+		}
+	}
+}
+
+void
+vmprint(pagetable_t pagetable){
+	printf("page table %p\n", pagetable);
+	vmprint_helper(pagetable, 0);
+}
